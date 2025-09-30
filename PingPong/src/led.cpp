053540b6f1f2bcb9c18cd12afd13f8f33b03f9bc
Kilian Hornischer <kilian.hornischer@gmx.at>
2025-09-30 10:23:56 +0200

    led test programm

--- PingPong/src/led.cpp
+++ PingPong/src/led.cpp
@@ -1,25 +1,29 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
-#define NUM_LEDS 30
-#define DATA_PIN 6
-
-CRGB leds[NUM_LEDS];
+const int numLeds = 8;
+const int ledPins[] = {2, 3, 4, 5, 6, 7, 8, 9};
 
 void setup() {
-  FastLED.addLeds<WS2811, DATA_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(50);
+  for (int i = 0; i < numLeds; i++) {
+    pinMode(ledPins[i], OUTPUT);
+  }
 }
 
 void loop() {
-  leds[0] = CRGB::Red;
++  for (int i = 0; i < numLeds; i++) {
+    for (int j = 0; j < numLeds; j++) {
+      digitalWrite(ledPins[j], LOW);
+    }
+    digitalWrite(ledPins[i], HIGH);
+    delay(100);
+  }
   
-  leds[NUM_LEDS-1] = CRGB::Blue;
-  
-  FastLED.show();
-  delay(1000);
-  
-  fill_solid(leds, NUM_LEDS, CRGB::Black);
-  FastLED.show();
-  delay(1000);
+  for (int i = numLeds - 1; i >= 0; i--) {
+    for (int j = 0; j < numLeds; j++) {
+      digitalWrite(ledPins[j], LOW);
+    }
+    digitalWrite(ledPins[i], HIGH);
+    delay(100);
+  }
 }
