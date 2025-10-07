88a2a35b97abfd209381c4af8c1763cd294d17ed
Kilian Hornischer <kilian.hornischer@gmx.at>
2025-10-07 11:08:36 +0200

    led runlight

--- PingPong/src/led.cpp
+++ PingPong/src/led.cpp
@@ -2,15 +2,64 @@
 #include <FastLED.h>
 
 #define NUM_LEDS 60
-#define DATA_PIN D3
+#define DATA_PIN D1
 
 CRGB leds[NUM_LEDS];
+
+unsigned long previousMillis = 0;
+const long interval = 50;
+
+int currentPosition = 0;
+bool reverseDirection = false;
+
+enum LEDState {
+  RUNLIGHT_ACTIVE
+};
+LEDState currentState = RUNLIGHT_ACTIVE;
 
 void setup() {
   FastLED.addLeds<WS2811, DATA_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setBrightness(50);
+  
+  FastLED.clear();
+  FastLED.show();
 }
 
 void loop() {
-  leds[0] = CRGB::Red;
+  unsigned long currentMillis = millis();
+  
+  switch (currentState) {
+    case RUNLIGHT_ACTIVE:
+      if (currentMillis - previousMillis >= interval) {
+        FastLED.clear();
+        
+        leds[currentPosition] = CRGB::Red;
+        
+        for (int i = 1; i <= 3; i++) {
+          int trailPos = currentPosition - i;
+          if (trailPos >= 0) {
+            leds[trailPos] = CRGB(255/(i*2), 0, 0);
+          }
+        }
+        
+        FastLED.show();
+        
+        if (!reverseDirection) {
+          currentPosition++;
+          if (currentPosition >= NUM_LEDS) {
+            reverseDirection = true;
+            currentPosition = NUM_LEDS - 2;
+          }
+        } else {
+          currentPosition--;
+          if (currentPosition < 0) {
+            reverseDirection = false;
+            currentPosition = 1;
+          }
+        }
+        
+        previousMillis = currentMillis;
+      }
+      break;
+  }
 }
